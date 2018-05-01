5ef975547b9a8bc13d1b1bc882f9c742395d2fbc
luoke <luokes@gmail.com>
2018-05-01 20:22:36 +0800

    add about

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,7 @@ Rails.application.routes.draw do
   resources :posts do
     resources :comments
   end
+
+  get 'about', to: 'abouts#index', as: 'about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
